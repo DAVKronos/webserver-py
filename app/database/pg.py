7983708f035fa8a21c61aa4f9f147bc238801c24
Maurice van Leeuwen <maurice.vanleeuwen@gmail.com>
2024-01-31 22:54:37 +0100

    Connect to correct pg database host inside Docker container

--- app/database/pg.py
+++ app/database/pg.py
@@ -3,6 +3,13 @@ from databases import Database
 
 async def get_database():
     async with Database(
+        "postgresql+asyncpg://postgres:kronos@db:5432/kronos_production"
+    ) as db:
+        yield db
+
+
+async def get_database():
+    async with Database(
         "postgresql+asyncpg://postgres:kronos@localhost:5432/kronos_production"
     ) as db:
         yield db
