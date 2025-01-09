d55eb0c18d06e1074cecc0a20fb25db019394270
Wessel Witteveen <Witteveen.w-1@student.utwente.nl>
2025-01-09 21:04:33 +0100

    Results get endpoints

--- app/models/result.py
+++ app/models/result.py
@@ -1,22 +1,22 @@
-from sqlmodel import Field, Relationship,  SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 from datetime import datetime
 
 class ResultBase(SQLModel):
     id: int | None
     created_at: datetime | None
-    updated_at: datetime | None
+    updated_at: datetime
     result: str | None
     username: str | None
     calculated: float | None
     wind: float | None
     place: int | None
-    event_id: int | None
     user_id: int | None
+    event_id: int | None
+
+class Result(ResultBase, table=True):
+    __tablename__ = "results"
+    id: int | None = Field(default=None, primary_key=True)
+
 
 class ResultResponse(ResultBase):
     pass
-
-class Result(ResultBase, table=True):
-    __tablename__: str = "results"
-    id: int | None = Field(default=None, primary_key=True)
-
--- app/routers/results.py
+++ app/routers/results.py
@@ -1,14 +1,25 @@
 from typing import Annotated
-from fastapi import APIRouter, Request, Depends
+from datetime import datetime
+from fastapi import APIRouter, Request, Depends, Query, HTTPException
+from sqlmodel import SQLModel, select
 from ..dependencies import Database
 from ..models.result import *
 
 router = APIRouter(prefix="/results")
 
-@router.get("", response_model=list[ResultResponse])
-async def get_all(r: Request, database: Database):
+@router.get("")
+async def get_all(r: Request, db: Database):
     query = select(Result) \
         .order_by(Result.created_at.desc())
 
-    results = await database.exec(query)
-    return results.all()
+    results = await db.exec(query)
+    return [ResultResponse.model_validate(result) for result in results.all()]
+
+
+
+@router.get("/{id}", response_model=ResultResponse)
+async def get(id: int, r: Request, db: Database):
+    result = await db.get(Result, id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Result not found")
+    return ResultResponse.model_validate(result)
