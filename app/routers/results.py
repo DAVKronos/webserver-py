4642c277e9f26a3bfe43a089f08f696f4bf35a51
Wessel Witteveen <Witteveen.w-1@student.utwente.nl>
2025-01-09 21:43:36 +0100

    Basis voor filteren op jaar of maand. Maar de endpoint moet eigenlijk iets anders terugsturen

--- app/routers/results.py
+++ app/routers/results.py
@@ -1,17 +1,28 @@
 from typing import Annotated
 from datetime import datetime
 from fastapi import APIRouter, Request, Depends, Query, HTTPException
-from sqlmodel import SQLModel, select
+from sqlmodel import SQLModel, select, func
+from sqlalchemy import extract
 from ..dependencies import Database
 from ..models.result import *
 
 router = APIRouter(prefix="/results")
 
 @router.get("")
-async def get_all(r: Request, db: Database):
+async def get_all(
+    r: Request, 
+    db: Database,
+    year: Annotated[int | None, Query(alias="date[year]")] = None,
+    month: Annotated[int | None, Query(alias="date[month]")] = None
+):
+    
     query = select(Result) \
         .order_by(Result.created_at.desc())
-
+    
+    if year:
+        query = query.where(extract('year', Result.updated_at) == year)
+    if month:
+        query = query.where(extract('month', Result.updated_at) == month)
     results = await db.exec(query)
     return [ResultResponse.model_validate(result) for result in results.all()]
 
