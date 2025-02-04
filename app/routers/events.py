4903bae6247c66ba32d33fb9b9bad6f907622bd3
Wessel Witteveen <witteveenwessel@gmail.com>
2025-02-04 23:44:47 +0100

    Removed print

--- app/routers/events.py
+++ app/routers/events.py
@@ -21,7 +21,6 @@ async def get_all(r: Request, database: Database):
 @router.get("/{id}", response_model=EventResponse)
 async def get(id: int, r: Request, db: Database):
     event = await db.get(Event, id)
-    print(event)
     if not event:
         raise HTTPException(status_code=404, detail="Event not found")
     return event
