73c7b82b74b4cb0ca7b65832977e809b0b616cd6
Wessel Witteveen <witteveenwessel@gmail.com>
2025-02-05 17:34:08 +0100

    Updated the user model and implemented /birthdays

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,15 @@
 from sqlmodel import Field, Relationship,  SQLModel
-from datetime import datetime
+from datetime import date, datetime
 
 class UserBase(SQLModel):
     name: str
+    initials: str
+    email: str
+    birthdate: date | None
+    created_at: datetime
+    updated_at: datetime
+    user_type_id: int
+    encrypted_password: str
 
 class UserResponse(UserBase):
     id: int | None
@@ -10,15 +17,7 @@ class UserResponse(UserBase):
     
 class User(UserBase, table=True):
     __tablename__: str = "users"
-    
     id: int | None = Field(default=None, primary_key=True)
-    created_at: datetime
-    updated_at: datetime
-    user_type_id: int
-    name: str
-    initials: str
-    email: str
-    encrypted_password: str
     articles: list["Article"] = Relationship(back_populates="user")
     comments: list["Comment"] = Relationship(back_populates="user")
     commission_memberships: list["CommissionMembership"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 from fastapi import APIRouter, Request, Depends, HTTPException
-from sqlmodel import select
+from sqlalchemy.sql import extract
+from sqlmodel import select, or_
 from ..dependencies import Database
+from datetime import date
 from ..models.user import *
 
 router = APIRouter(prefix="")
@@ -10,16 +12,14 @@ router = APIRouter(prefix="")
 async def get_all(r: Request, database: Database):
     query = select(User) \
         .order_by(User.name.desc())
-
+        #The users table currently has 2 problematic entries, id 479 & 340
+        # these have a birthdate with the year 0001 which might return errors
+        # after manually changing it and setting it back it somehow works again
+        # but maybe update 01/01/0001 dates?
     users = await database.exec(query)
     return users.all()
 
-@router.get("/users/{id}", response_model=UserResponse)
-async def get_one(id: int, r: Request, database: Database):
-    user = database.get(User, id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+
 
 @router.get("/user_types", response_model=list[UserTypeResponse])
 async def get_all_usertypes(r: Request, database: Database):
@@ -31,12 +31,30 @@ async def get_all_usertypes(r: Request, database: Database):
 
 @router.get("/user_types/{id}", response_model=UserTypeResponse)
 async def get_one_usertype(id: int, r: Request, database: Database):
-    usertype = database.get(UserType, id)
+    usertype = await database.get(UserType, id)
     if not usertype:
         raise HTTPException(status_code=404, detail="Usertype not found")
     return usertype
 
 @router.get("/users/birthdays", response_model=list[UserResponse])
 async def get_birthdays(r: Request, database: Database):
-    #Implement the birthdays
-    pass
+    today = date.today()
+    next_month = (today.month % 12) + 1
+    query = select(User) \
+        .where(
+            User.user_type_id != 9,
+            User.birthdate.is_not(None),
+            or_(
+                (extract("month", User.birthdate) == today.month) & (extract("day", User.birthdate) >= today.day),
+                (extract("month", User.birthdate) == next_month) & (extract("day", User.birthdate) <= today.day)
+            )
+            )
+    users = await database.exec(query)
+    return users.all()
+
+@router.get("/users/{id}", response_model=UserResponse)
+async def get_one(id: int, r: Request, database: Database):
+    user = await database.get(User, id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
