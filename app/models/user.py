e335d1efd0814f01edfe05f8a59d72e7620e3086
Wessel Witteveen <witteveenwessel@gmail.com>
2025-02-05 18:00:24 +0100

    Some more model addtions but images still broken

--- app/models/user.py
+++ app/models/user.py
@@ -8,12 +8,15 @@ class UserBase(SQLModel):
     birthdate: date | None
     created_at: datetime
     updated_at: datetime
+    avatar_file_name: str | None
+    avatar_content_type: str | None
+    avatar_file_size: int | None
+    avatar_updated_at: datetime | None
     user_type_id: int
     encrypted_password: str
 
 class UserResponse(UserBase):
     id: int | None
-    email: str
     
 class User(UserBase, table=True):
     __tablename__: str = "users"
