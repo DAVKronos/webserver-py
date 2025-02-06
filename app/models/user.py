69f8924421d057b717855e56959e337f24c39633
Wessel Witteveen <witteveenwessel@gmail.com>
2025-02-06 11:33:02 +0100

    Updated user model for basic user profile

--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,35 @@
 from sqlmodel import Field, Relationship,  SQLModel
 from datetime import date, datetime
 
+
 class UserBase(SQLModel):
     name: str
     initials: str
     email: str
     birthdate: date | None
+    address: str | None
+    postalcode: str | None
+    city: str | None
+    sex: str | None
+    licensenumber: str | None
+    papieren_kronometer: bool | None
     created_at: datetime
     updated_at: datetime
     avatar_file_name: str | None
     avatar_content_type: str | None
     avatar_file_size: int | None
     avatar_updated_at: datetime | None
-    user_type_id: int
     encrypted_password: str
+    phonenumber: str | None
+    user_type_id: int
+    xtracard: str | None
+    studie: str | None
+    instelling: str | None
+    aanvang: int | None
 
 class UserResponse(UserBase):
     id: int | None
+    commissions: list["CommissionResponse"] = []
     
 class User(UserBase, table=True):
     __tablename__: str = "users"
@@ -25,7 +38,6 @@ class User(UserBase, table=True):
     comments: list["Comment"] = Relationship(back_populates="user")
     commission_memberships: list["CommissionMembership"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
     subscriptions: list["Subscription"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
-
 
 class UserTypeBase(SQLModel):
     name: str | None
@@ -42,3 +54,5 @@ class UserType(UserTypeBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     created_at: datetime
     updated_at: datetime
+
+from .commissions import CommissionResponse
