707a183e8ab4b09c475776975920218af28f783b
Milan Veul <milan.veul@gmail.com>
2026-01-01 21:44:40 +0100

    Added support for dual connection (with old and new db)

--- app/data_migration/test.py
+++ app/data_migration/test.py
@@ -3,14 +3,16 @@ import asyncio
 from sqlalchemy import text
 from ..database import create_ssh_engine
 
-async def run_with_connection(func, database_name):
-    with create_ssh_engine(database_name) as engine:
-        async with engine.connect() as conn:
-            await func(conn)
+async def run_with_connection(func, old_db_name, new_db_name):
+    with create_ssh_engine(old_db_name) as old_engine, \
+         create_ssh_engine(new_db_name) as new_engine:
+        async with old_engine.connect() as old_conn:
+            async with new_engine.connect() as new_conn:
+                await func(old_conn, new_conn)
 
 # Test function 
-async def print_users(conn):
-    result = await conn.execute(text("SELECT * FROM users"))
+async def print_users(old_conn, new_conn):
+    result = await old_conn.execute(text("SELECT * FROM users"))
     rows = [dict(row) for row in result.mappings().all()]
     print(rows)
 
