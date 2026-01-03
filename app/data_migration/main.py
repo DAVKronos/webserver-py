012f30669fd010bed4d8ba54ff569e20b9457c2f
Milan Veul <milan.veul@gmail.com>
2026-01-03 22:43:38 +0100

    Database migration code

--- app/data_migration/main.py
+++ app/data_migration/main.py
@@ -1,24 +1,333 @@
-### TO RUN THIS FILE: docker compose run --rm python python3 -m app.data_migration.main
-import asyncio
+# TO RUN THIS FILE  : docker compose run --rm python python3 -m app.data_migration.main
 
+import asyncio
+from datetime import datetime
 from sqlalchemy import text
 from ..database import create_ssh_engine
+import app.data_migration.migration_helper as helper
 
-async def run_with_connection(func, old_db_name, new_db_name):
+async def empty_db(conn):
+    # Empty the new database first
+    print("Emptying the new database...")
+    await conn.execute(text("""
+        TRUNCATE 
+            agendaitem_types, committees, files, mail_aliases, pages, 
+            photo_tags, user_types, document_folders, documents, 
+            mailing_lists, users, agendaitems, committee_members, 
+            mailing_list_members, photo_albums, photos, 
+            reset_password_actions, subscriptions, announcements, 
+            has_tags, news_items, news_comments 
+        RESTART IDENTITY CASCADE;
+    """))
+    print("Database cleared.")
+
+async def migrate(old_conn, new_conn):
+    await empty_db(new_conn)
+
+    photo_tags_mapping = {
+        "id": "id",
+        "name": "name"
+    }
+    await helper.migrate_table(old_conn, new_conn, "tag_", "photo_tags", photo_tags_mapping)
+
+    pages_mapping = {
+    "id": "id",
+    "information": "content_nl",
+    "information_en": "content_en",
+    "pagetag": "page_title_nl",
+    "pagetag_en": "page_title_en",
+    "menu": "menu_item",
+    "highlight": "is_highlight",
+    "public": "is_public",
+    "created_at": "created_at",
+    "updated_at": "updated_at"
+}
+    await helper.migrate_table(old_conn, new_conn, "pages", "pages", pages_mapping)
+
+    mail_alias_mapping = {
+        "id": "id",
+        "name": "name",
+        "emailaddress": "email_address",
+        "description": "description",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    await helper.migrate_table(old_conn, new_conn, "aliases", "mail_aliases", mail_alias_mapping)
+
+    user_types_mapping = {
+        "id": "id", 
+        "name": "name_nl", 
+        "name_en": "name_en", 
+        "donor": "is_donor", 
+        "competition": "is_competition", 
+        "created_at": "created_at", 
+        "updated_at": "updated_at"
+    }
+    await helper.migrate_table(old_conn, new_conn, "user_types", "user_types", user_types_mapping)
+
+    agendaitems_types_mapping = {
+        "id": "id", 
+        "name": "name_nl", 
+        "name_en": "name_en", 
+        "created_at": "created_at", 
+        "updated_at": "updated_at"
+    }
+    await helper.migrate_table(old_conn, new_conn, "agendaitemtypes", "agendaitem_types", agendaitems_types_mapping)
+    
+    committees_mapping = {
+        "id": "id", 
+        "name": "name_nl", 
+        "name_en": "name_en", 
+        "description": "description_nl", 
+        "description_en": "description_en", 
+        "email": "email", 
+        "role": "role", 
+        "created_at": "created_at", 
+        "updated_at": "updated_at"
+    }
+    await helper.migrate_table(old_conn, new_conn, "commissions", "committees", committees_mapping)
+
+    user_mapping = {
+        "id": 'id',
+        "name": 'name',
+        "initials": 'initials',
+        "email": 'email',
+        "encrypted_password": "password",
+        "birthdate": 'birthdate',
+        "address": 'address',
+        "postalcode": 'postalcode',
+        "city": 'city',
+        "sex": 'sex',
+        "allow_password_change": 'allow_password_change',
+        "phonenumber": 'phonenumber',
+        "user_type_id": 'user_type_id', 
+        "bank_account_number": 'bank_account_number',
+        "xtracard": 'unioncard_number',
+        "instelling": 'institution',
+        "aanvang": 'joined_in',
+        "tokens": 'tokens',
+        "confirmed_at": 'confirmed_at',
+        "created_at": 'created_at' or datetime.now(),
+        "updated_at": 'updated_at' or datetime.now()
+    }
+    await helper.migrate_table(old_conn, new_conn, "users", "users", user_mapping, {}, helper.user_criteria)
+
+    announcement_mapping = {
+        "id": "id",
+        "title": "title",
+        "message": "content",
+        "user_id": "user_id",
+        "url": "url",
+        "starts_at": "starts_at",
+        "ends_at": "ends_at",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    announcement_fk_mapping = {
+        "user_id": "users"
+    }
+    await helper.migrate_table(old_conn, new_conn, "announcements", "announcements", announcement_mapping, announcement_fk_mapping)
+
+    mailing_list_mapping = {
+        "id": "id",
+        "name": "name",
+        "description": "description",
+        "address": "email_address",
+        "local_part": "local_part",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    await helper.migrate_table(old_conn, new_conn, "mailinglists", "mailing_lists", mailing_list_mapping)
+
+    mailing_list_members_mapping = {
+        "id": "id",
+        "mailinglist_id": "mailing_list_id",
+        "user_id": "user_id",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    mailing_list_members_fk_mapping = {
+        "mailing_list_id": "mailing_lists",
+        "user_id": "users"
+    }
+    await helper.migrate_table(old_conn, new_conn, "mailinglist_memberships", "mailing_list_members", mailing_list_members_mapping, mailing_list_members_fk_mapping)
+
+    committee_members_map = {
+        "id": "id",
+        "function": "function",
+        "user_id": "user_id",
+        "commission_id": "committee_id",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    committee_members_fk_map = {
+        "user_id": "users",
+        "committee_id": "committees"
+    }
+    await helper.migrate_table(old_conn, new_conn, "commission_memberships", "committee_members", committee_members_map, committee_members_fk_map)
+
+    agenda_items_mapping = {
+        "id": "id",
+        "name": "name_nl",
+        "name_en": "name_en",
+        "description": "description_nl",
+        "description_en": "description_en",
+        "date": "date",
+        "location": "location",
+        "commission_id": "committee_id",
+        "intern": "is_internal",
+        "agendaitemtype_id": "agendaitem_type_id",
+        "url": "url",
+        "user_id": "created_by_user_id",
+        "subscribe": "can_subscribe",
+        "subscriptiondeadline": "subscription_deadline",
+        "maxsubscription": "max_subscriptions",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    agenda_items_fk_mapping = {
+        "committee_id": "committees",
+        "agendaitem_type_id": "agendaitem_types",
+        "created_by_user_id": "users"
+    }
+    await helper.migrate_table(old_conn, new_conn, "agendaitems", "agendaitems", agenda_items_mapping, agenda_items_fk_mapping)
+
+    news_items_mapping = {
+        "id": "id",
+        "title": "title_nl",
+        "title_en": "title_en",
+        "news": "content_nl",
+        "news_en": "content_en",
+        "agreed": "approved",
+        "agreed_by": "approved_by",
+        "user_id": "creator_id",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    news_items_fk_mapping = {
+        "approved_by": "users",
+        "creator_id": "users"
+    }
+    await helper.migrate_table(old_conn, new_conn, "newsitems", "news_items", news_items_mapping, news_items_fk_mapping)
+
+    news_comments_mapping = {
+        "id": "id",
+        "user_id": "user_id",
+        "newsitem_id": "newsitem_id",
+        "commenttext": "content",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    news_comments_fk_mapping = {
+        "user_id": "users",
+        "newsitem_id": "news_items"
+    }
+    await helper.migrate_table(old_conn, new_conn, "comments", "news_comments", news_comments_mapping, news_comments_fk_mapping)
+
+    subscription_mapping = {
+        "id": "id",
+        "user_id": "user_id",
+        "comment": "comment",
+        "agendaitem_id": "agendaitem_id",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    subscription_fk_mapping = {
+        "user_id": "users",
+        "agendaitem_id": "agendaitems"
+    }
+    await helper.migrate_table(old_conn, new_conn, "subscriptions", "subscriptions", subscription_mapping, subscription_fk_mapping)
+    
+    photo_album_mapping = {
+        "id": "id",
+        "name": "name_nl",
+        "name_en": "name_en",
+        "agendaitem_id": "agendaitem_id",
+        "public": "is_public",
+        "event_date": "event_date",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    photo_album_fk_mapping = {
+        "agendaitem_id": "agendaitems"
+    }
+    await helper.migrate_table(old_conn, new_conn, "photoalbums", "photo_albums", photo_album_mapping, photo_album_fk_mapping)
+
+    document_folder_mapping = {
+        "id": "id",
+        "name": "name",
+        "folder_id": "parent_folder_id"
+    }
+    await helper.migrate_self_referencing_table(old_conn, new_conn, "folders", "document_folders", document_folder_mapping, "parent_folder_id")
+
+    document_mapping = {
+        "id": "id",
+        "name": "name",
+        "date": "date",
+        "folder_id": "folder_id",
+        "public": "is_public",
+        "created_at": "created_at",
+        "updated_at": "updated_at"
+    }
+    await helper.migrate_table(old_conn, new_conn, "kronometers", "documents", document_mapping)
+
+    user_avatar_field_names = {
+        "file_name": "avatar_file_name",
+        "content_type": "avatar_content_type",
+        "file_size": "avatar_file_size",
+        "updated_at": "avatar_updated_at"
+    }
+    await helper.migrate_table_file(old_conn, new_conn, "users", "users", user_avatar_field_names, "avatar_file_id")
+
+    newsitem_file_field_names = {
+        "file_name": "articlephoto_file_name",
+        "content_type": "articlephoto_content_type",
+        "file_size": "articlephoto_file_size",
+        "updated_at": "articlephoto_updated_at"
+    }
+    await helper.migrate_table_file(old_conn, new_conn, "newsitems", "news_items", newsitem_file_field_names, "photo_file_id")
+
+    announcement_file_field_names = {
+        "file_name": "background_file_name",
+        "content_type": "background_content_type",
+        "file_size": "background_file_size",
+        "updated_at": "background_updated_at"
+    }
+    await helper.migrate_table_file(old_conn, new_conn, "announcements", "announcements", announcement_file_field_names, "photo_file_id")
+
+    document_file_field_names = {
+        "file_name": "file_file_name",
+        "content_type": "file_content_type",
+        "file_size": "file_file_size",
+        "updated_at": "file_updated_at"
+    }
+    await helper.migrate_table_file(old_conn, new_conn, "kronometers", "documents", document_file_field_names, "file_id")
+
+    # A lot of refs are now invalid because the photoalbums table in the test database has been whiped,
+    # so there are a lot of photos missing. This will be different on the prod database
+    await helper.migrate_photos(old_conn, new_conn)
+
+    
+    print("Finished migration.")
+
+# Setup dual connection
+async def run_with_dual_connection(func, old_db_name, new_db_name):
     with create_ssh_engine(old_db_name) as old_engine, \
          create_ssh_engine(new_db_name) as new_engine:
         async with old_engine.connect() as old_conn:
             async with new_engine.connect() as new_conn:
                 await func(old_conn, new_conn)
+                await new_conn.commit()
 
-async def migrate(old_conn, new_conn):
-    await migrate_users(old_conn, new_conn)
-
-async def migrate_users(old_conn, new_conn):
-    result = await old_conn.execute(text("SELECT * FROM users"))
-    rows = [dict(row) for row in result.mappings().all()]
-    print(rows)
-
-# Run the migration
-# May need to empty the new database first
-asyncio.run(run_with_connection(migrate, 'database_old', 'database'))
+# Start migration
+if __name__ == "__main__":
+    print("""
+It is EXTREMELY important that your config.toml references the correct databases.  
+    > database_old    should be referring to the db FROM which the migration happens.
+    > database        should be referring to the db TO which the migration happens.
+!! This database will be EMPTIED before migration starts. !!
+Have you checked your config and are you sure to continue? [Y/N]
+          """)
+    res = str(input())
+    if (res == "Y"):
+        asyncio.run(run_with_dual_connection(migrate, 'database_old', 'database'))
---
+++ app/data_migration/migration_helper.py
@@ -0,0 +1,181 @@
+import re
+from datetime import datetime
+from sqlalchemy import text
+
+async def migrate_table(old_conn, new_conn, old_table, new_table, col_mapping, fk_mapping={}, migration_criteria=None):
+    print(f"Starting {new_table} migration...")
+    res = await old_conn.execute(text(f"SELECT * FROM {old_table}"))
+    rows = res.mappings().all()
+    
+    if not rows:
+        print(f"No rows found in {old_table}. Skipping.")
+        return
+
+    valid_refs = await get_valid_references(new_conn, fk_mapping)
+    to_insert = []
+    invalid_ref_count = 0
+
+    for row in rows:
+        # Filter data if a function is given
+        if migration_criteria and not migration_criteria(row):
+            continue # Do not migrate this row
+        
+        # Invert mapping: map[old_column] = new_column
+        data = {new_k: row[old_k] for old_k, new_k in col_mapping.items() if old_k in row}
+
+        # TODO: Maybe have a better policy for non-existing references
+        for fk_col, ids in valid_refs.items():
+            if data.get(fk_col) not in ids:
+                invalid_ref_count += 1
+                data[fk_col] = None
+        
+        to_insert.append(data)
+
+    await bulk_insert(new_conn, to_insert, new_table)
+    
+    await update_sequence(new_conn, new_table)
+    print(f"Finished {new_table} migration. Inserted {len(to_insert)} rows with {invalid_ref_count} invalid refs.")
+
+async def migrate_self_referencing_table(old_conn, new_conn, old_table, new_table, col_mapping, self_mapping_col):
+    print(f"Starting self-referencing {new_table} migration...")
+    res = await old_conn.execute(text(f"SELECT * FROM {old_table} ORDER BY id ASC"))
+    rows = res.mappings().all()
+    invalid_ref_count = 0
+    valid_ids = set()
+    to_insert = []
+
+    for row in rows:
+        data = {new_k: row[old_k] for old_k, new_k in col_mapping.items() if old_k in row}
+        
+        # Check if the self_mapping_col exists in the set
+        parent_id = data.get(self_mapping_col)
+        if parent_id and parent_id not in valid_ids:
+            print(f"Parent {parent_id} not found for {data['id']}. Setting to NULL.")
+            data[self_mapping_col] = None
+            invalid_ref_count += 1
+        to_insert.append(data)
+        valid_ids.add(data["id"])
+
+    if len(to_insert) != 0:
+        await bulk_insert(new_conn, to_insert, new_table)
+
+    await update_sequence(new_conn, "document_folders")
+    print(f"Finished {new_table} migration. Inserted {len(to_insert)} rows with {invalid_ref_count} invalid refs.")
+
+async def get_valid_references(conn, fk_mapping):
+    valid_refs = {}
+    for col, ref_table in fk_mapping.items():
+        ref_res = await conn.execute(text(f"SELECT id FROM {ref_table}"))
+        valid_refs[col] = {r[0] for r in ref_res.fetchall()} # Save valid id's for each ref
+        valid_refs[col].add(None) # count NULL ref as valid ref
+    return valid_refs
+
+async def bulk_insert(conn, to_insert, table):
+    cols = ", ".join(to_insert[0].keys())
+    placeholders = ", ".join([f":{k}" for k in to_insert[0].keys()])
+    stmt = text(f"INSERT INTO {table} ({cols}) VALUES ({placeholders})")
+    await conn.execute(stmt, to_insert)
+
+async def update_sequence(conn, table):
+    try:
+        await conn.execute(text(f"SELECT setval(pg_get_serial_sequence('{table}', 'id'), COALESCE((SELECT MAX(id) FROM {table}), 1))"))
+    except Exception as e:
+        print(f"Warning: Could not update sequence: {e}")
+
+# Do not migrate deleted users
+def user_criteria(row):
+    regex = r"x\d+.*" # Filter strings like x70 (may have a suffix)
+    return not bool(re.fullmatch(regex, row["name"]))
+
+# Create a file for each row and updating the corresponding row's file_id
+async def migrate_table_file(old_conn, new_conn, old_table, new_table, field_names, file_id_field):
+    print(f"Starting {new_table} file migration...")
+    
+    res = await old_conn.execute(text(f"SELECT id, {', '.join(field_names.values())} FROM {old_table}"))
+    rows = res.mappings().all()
+    rows_with_file = [r for r in rows if r[field_names["file_name"]] is not None]
+    
+    if not rows_with_file:
+        return
+
+    new_file_ids = await upload_files(new_conn, rows_with_file, field_names)
+    
+    updates = [
+        {"table_id": rows_with_file[i]["id"], "file_id": new_file_ids[i]}
+        for i in range(len(rows_with_file))
+    ]
+
+    update_stmt = text(f"UPDATE {new_table} SET {file_id_field} = :file_id WHERE id = :table_id")
+    await new_conn.execute(update_stmt, updates)
+
+    print(f"Finished {new_table} photo migration. Inserted {len(new_file_ids)} files and updated {len(updates)} rows.")
+
+async def migrate_photos(old_conn, new_conn):
+    print("Starting photo migration...")
+    res = await old_conn.execute(text("SELECT * FROM photos"))
+    rows = res.mappings().all()
+
+    field_names = {
+        "file_name": "photo_file_name",
+        "content_type": "photo_content_type",
+        "file_size": "photo_file_size",
+        "updated_at": "photo_updated_at"
+    }
+    new_file_ids = await upload_files(new_conn, rows, field_names)
+
+    valid_refs = await get_valid_references(new_conn, {"photoalbum_id": "photo_albums"})
+    invalid_ref_count = 0
+
+    to_insert = []
+    for i, row in enumerate(rows):
+        data = {
+            "id": row["id"],
+            "file_id": new_file_ids[i],
+            "photoalbum_id": row["photoalbum_id"],
+            "exif_date": row["exif_date"],
+            "url": row["photo_url_original"],
+            "thumbnail_url": row["photo_url_thumb"],
+            "created_at": row["created_at"],
+            "updated_at": row["updated_at"]
+        }
+
+        if data["photoalbum_id"] not in valid_refs["photoalbum_id"]:
+            data["photoalbum_id"] = None
+            invalid_ref_count += 1
+        
+        to_insert.append(data)
+    
+    await bulk_insert(new_conn, to_insert, "photos")
+    print(f"Finished photo migration. Inserted {len(new_file_ids)} files and {len(to_insert)} photos with {invalid_ref_count} invalid refs.")
+
+async def upload_files(conn, rows, field_names):
+    chunk_size = 5000
+    all_new_ids = []
+    
+    # We apparently need to process rows in chunks to avoid a 32767 parameter limit
+    for i in range(0, len(rows), chunk_size):
+        chunk = rows[i : i + chunk_size]
+        params = {}
+        value_strings = []
+        
+        for j, r in enumerate(chunk):
+            # Unique keys within this specific chunk
+            params.update({
+                f"fn_{j}": r[field_names["file_name"]],
+                f"ct_{j}": r[field_names["content_type"]],
+                f"fs_{j}": r[field_names["file_size"]],
+                f"ua_{j}": r[field_names["updated_at"]]
+            })
+            value_strings.append(f"(:fn_{j}, :ct_{j}, :fs_{j}, :ua_{j})")
+
+        stmt_text = f"""
+            INSERT INTO files (file_name, content_type, file_size, updated_at) 
+            VALUES {', '.join(value_strings)} 
+            RETURNING id
+        """
+        
+        result = await conn.execute(text(stmt_text), params)
+        chunk_ids = [r[0] for r in result.all()]
+        all_new_ids.extend(chunk_ids)
+        
+    return all_new_ids
