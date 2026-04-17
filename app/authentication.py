6b597671b8c535ad3c41b1a6b521182ec747b635
MilanVeul <milan@veul.net>
2026-04-17 14:48:57 +0200

    Add usercreate

--- app/authentication.py
+++ app/authentication.py
@@ -77,6 +77,8 @@ async def login(database, username, password):
 
     return create_token(str(user.id))
 
+
+
 async def get_current_user(
     database: Database, 
     security_scopes: SecurityScopes, 
