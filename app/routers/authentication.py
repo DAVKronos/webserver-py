04706395b33e6cccc9dce2819b3c9f19994e408c
MilanVeul <milan@veul.net>
2026-04-17 14:08:19 +0200

    Remove old permission system

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -33,11 +33,11 @@ async def login(
 @router.post("/logout")
 async def logout():
     # Backend is stateless, so logging out is primarily a frontend action
-    # => No need to call this endpoint
+    # => No need for calling this endpoint
     return {"detail": "Successfully logged out"}
 
 @router.get("/current_user", response_model=User)
-async def get_current_user(current_user: Annotated[Optional[User], Depends(current_user)]):
+async def get_current_user(current_user: Annotated[Optional[User], Depends(get_current_user)]):
     return User.model_validate(current_user)
 
 
@@ -62,57 +62,57 @@ async def reset_password():
     return Response(200)
 
 
-# TODO: don't need this anymore when the JWT contains scopes
-@router.get("/permissions", response_model= list[Ability], response_model_exclude_none=True)
-async def permissions(request: Request, database: Database, user: Annotated[Optional[User], Depends(current_user)]):
-    everyone = [can('read', 'all'),
-                can(['home', 'titleshow'], 'Page'),
-                can(['game'], 'Page'),
-                cannot('read', 'Page'),
-                can('read', 'Page', {'public':True}),
-                can(['perdag', 'month'], 'Agendaitem'),
-                can(['frontpage', 'records'], 'Result'),
-                can(['current', 'hide'], 'Announcement'),
-                can('create', ['Contact']),
-                cannot('read', 'Photoalbum'),
-                can('read', 'Photoalbum', {'public': True}),
-                cannot('read', ['User','Photo','Announcement', 'Kronometer', 'Subscription', 'Comment']),
-                can(['read','display'], 'Kronometer', {'public':True}),
-                cannot('see_email', 'Committee')]
+# # TODO: don't need this anymore when the JWT contains scopes
+# @router.get("/permissions", response_model= list[Ability], response_model_exclude_none=True)
+# async def permissions(request: Request, database: Database, user: Annotated[Optional[User], Depends(current_user)]):
+#     everyone = [can('read', 'all'),
+#                 can(['home', 'titleshow'], 'Page'),
+#                 can(['game'], 'Page'),
+#                 cannot('read', 'Page'),
+#                 can('read', 'Page', {'public':True}),
+#                 can(['perdag', 'month'], 'Agendaitem'),
+#                 can(['frontpage', 'records'], 'Result'),
+#                 can(['current', 'hide'], 'Announcement'),
+#                 can('create', ['Contact']),
+#                 cannot('read', 'Photoalbum'),
+#                 can('read', 'Photoalbum', {'public': True}),
+#                 cannot('read', ['User','Photo','Announcement', 'Kronometer', 'Subscription', 'Comment']),
+#                 can(['read','display'], 'Kronometer', {'public':True}),
+#                 cannot('see_email', 'Committee')]
 
-    abilities = [] + everyone
+#     abilities = [] + everyone
 
-    if user is not None:
-        abilities += [can('read', 'all'),
-                   can('read', 'Page'),
-                   can('see_email', 'Committee'),
-                   can('create', ['Photo','Newsitem','Agendaitem','Event','Result','Comment']),
-                   can(['archief','wedstrijden','new_result','create_result', 'icalendar', 'duplicate'], 'Agendaitem'),
-                   can(['read','create','update'], 'Photoalbum'),
-                   can(['create', 'update'], ['Subscription'], {'user_id': user.id}),
-                   can('display', 'Kronometer'),
-                   can('update', 'Agendaitem', {'user_id': user.id}),
-                   can(['update','editpassword'], 'User', {'id':user.id}),
-                   can('birthdays', 'User'),
-                   cannot('create', 'User')]
+#     if user is not None:
+#         abilities += [can('read', 'all'),
+#                    can('read', 'Page'),
+#                    can('see_email', 'Committee'),
+#                    can('create', ['Photo','Newsitem','Agendaitem','Event','Result','Comment']),
+#                    can(['archief','wedstrijden','new_result','create_result', 'icalendar', 'duplicate'], 'Agendaitem'),
+#                    can(['read','create','update'], 'Photoalbum'),
+#                    can(['create', 'update'], ['Subscription'], {'user_id': user.id}),
+#                    can('display', 'Kronometer'),
+#                    can('update', 'Agendaitem', {'user_id': user.id}),
+#                    can(['update','editpassword'], 'User', {'id':user.id}),
+#                    can('birthdays', 'User'),
+#                    cannot('create', 'User')]
 
-        abilities += [can('destroy', 'Subscription', {'id':sub.id}) for sub in user.subscriptions if sub.agendaitem.is_before_deadline()]
+#         abilities += [can('destroy', 'Subscription', {'id':sub.id}) for sub in user.subscriptions if sub.agendaitem.is_before_deadline()]
         
-        if len(user.Committee_memberships) > 0:
-            abilities += [can('manage', 'Agendaitem', {'user_id': user.id})]           
-            abilities += [can('update', 'Agendaitem', {'Committee_id': cm.Committee_id}) for cm in user.Committee_memberships]
+#         if len(user.Committee_memberships) > 0:
+#             abilities += [can('manage', 'Agendaitem', {'user_id': user.id})]           
+#             abilities += [can('update', 'Agendaitem', {'Committee_id': cm.Committee_id}) for cm in user.Committee_memberships]
             
-            for cm in user.Committee_memberships:
-                match cm.Committee.role:
-                    case "KRONOMETER_ADMIN":
-                        abilities += [can('kronometer_list', 'User'),
-                         can('manage', 'Kronometer')]
-                    case "RESULT_ADMIN":
-                        abilities += [can('manage', 'Result')]
-                    case "ADMIN":
-                        abilities += [can('manage', 'all'),
-                                      can(['update_mailinglists', 'update_announcements'], 'User'),
-                                      cannot('destroy', 'User')]
+#             for cm in user.Committee_memberships:
+#                 match cm.Committee.role:
+#                     case "KRONOMETER_ADMIN":
+#                         abilities += [can('kronometer_list', 'User'),
+#                          can('manage', 'Kronometer')]
+#                     case "RESULT_ADMIN":
+#                         abilities += [can('manage', 'Result')]
+#                     case "ADMIN":
+#                         abilities += [can('manage', 'all'),
+#                                       can(['update_mailinglists', 'update_announcements'], 'User'),
+#                                       cannot('destroy', 'User')]
             
     
-    return abilities
+#     return abilities
