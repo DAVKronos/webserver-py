c4639f06535a26f0853ea9db09260365808c44e2
MilanVeul <milan@veul.net>
2026-04-21 11:40:30 +0200

    Added pages routers

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -1,22 +1,88 @@
 from sqlmodel import select
-from typing import Annotated
-from fastapi import APIRouter, Request, HTTPException
+from typing import Annotated, Depends
+from fastapi import APIRouter, Request, HTTPException, status
 from sqlmodel import select
 from ..dependencies import Database
 from ..models.page import *
+from ..permissions import *
+from ..authentication import get_current_user, get_optional_user
+from ..time_utils import now
 
 router = APIRouter(prefix="/pages")
 
 @router.get("", response_model=list[PageResponse])
-async def get_all(r: Request, database: Database):
+async def get_all(r: Request, database: Database, user_context: Annotated[Optional[UserContext], Depends(get_optional_user)]):
     query = select(Page)
 
-    pages = await database.exec(query)
-    return pages.all()
+    pages: List[Page] = (await database.exec(query)).all()
+    if not user_context: # Not logged in
+        pages = filter(lambda page: page.is_public, pages)
+
+    return filter()
 
 @router.get("/{id}", response_model=PageResponse)
-async def get(id: int, r: Request, database: Database):
+async def get(id: int, r: Request, database: Database, user_context: Annotated[Optional[UserContext], Depends(get_optional_user)]):
     page = await database.get(Page, id)
     if not page:
         raise HTTPException(status_code=404, detail="Page not found")
+    if not user_context and not page.is_public:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Not enough permissions.")
     return page
+
+@router.post("", response_model=PageResponse)
+async def create_page(
+    data: PageCreate,
+    database: Database,
+    user_context: Annotated[UserContext, Depends(get_current_user)]
+):
+    if not user_can(user_context.permissions, CREATE, "Page"):
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+
+    t = now()
+    page = Page.model_validate(data, update={"created_at": t, "updated_at": t})
+
+    database.add(page)
+    await database.commit()
+    await database.refresh(page)
+
+    return page
+
+@router.patch("/{id}", reponse_model=PageResponse)
+async def update_page(
+    id: int,
+    data: PageUpdate,
+    database: Database,
+    user_context: Annotated[UserContext, Depends(get_current_user)]
+):
+    page = await database.get(Page, id)
+
+    if not page:
+        raise HTTPException(status_code=404, detail="Committee not found")
+    if not user_can(user_context.permissions, EDIT, "Page", page):
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+
+    update_data = data.model_dump(exclude_unset=True)
+    page.sqlmodel_update(update_data, update = {'updated_at': now()})
+
+    database.add(page)
+    await database.commit()
+    await database.refresh(page)
+
+    return page
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_page(
+    id: int,
+    database: Database,
+    user_context: Annotated[UserContext, Depends(get_current_user)]
+):
+    committee = await database.get(Committee, id)
+
+    if not committee:
+        raise HTTPException(status_code=404, detail="Committee not found")
+    if not user_can(user_context.permissions, DELETE, "Committee", committee):
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+
+    await database.delete(committee)
+    await database.commit()
+    return 
