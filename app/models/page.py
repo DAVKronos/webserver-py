96d84d94e1afc1ae633775db52108d41faabd295
MilanVeul <milan@veul.net>
2026-04-21 10:43:26 +0200

    Added PageCreate and PageUpdate model

--- app/models/page.py
+++ app/models/page.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlmodel import Field
+from sqlmodel import Field, SQLModel
 from .base import TimestampModel
 
 class PageBase(TimestampModel):
@@ -17,3 +17,21 @@ class Page(PageBase, table=True):
 
 class PageResponse(PageBase):
     id: int
+
+class PageCreate(SQLModel):
+    content_nl: str
+    content_en: str
+    page_title_nl: str
+    page_title_en: str
+    menu_item: Optional[str] = None
+    is_highlight: bool = False
+    is_public: bool = True
+
+class PageUpdate(SQLModel):
+    content_nl: Optional[str] = None
+    content_en: Optional[str] = None
+    page_title_nl: Optional[str] = None
+    page_title_en: Optional[str] = None
+    menu_item: Optional[str] = None
+    is_highlight: Optional[bool] = None
+    is_public: Optional[bool] = None
